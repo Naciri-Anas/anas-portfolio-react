9a8578cbbba44760b77d2f0f255a27e3c21074fc
Anas Naciri <110792944+Naciri-Anas@users.noreply.github.com>
2024-10-09 10:34:34 +0100

    today-update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <div className="flex flex-col items-center lg:items-start">
                 <h1 className="pb-16 text-6xl font-thin tracking-tighter lg:mt-16 lg:text-8xl">Anas Naciri
                 </h1>
-                <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
+                <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">Full Stack Developer</span>
                 <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
             </div>
         </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,35 @@
+import {PROJECTS} from "../constants";
 const Projects = () => {
   return (
+    <div className="border-b border-neutral-900 pb-4">
+    <h2 className="my-20 text-center text-4xl">
+      Projects
+    </h2>
     <div>
+      {PROJECTS.map((project,index)=>(
+        <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
+        <div className="w-full lg:w-1/4">
+    <img 
+        src={project.image} 
+        width={150} 
+        height={150} 
+        alt={project.title} 
+        className="mb-6 rounded transition-transform duration-300 transform hover:scale-105 hover:shadow-lg"
+    />
+</div>
+
+        <div className="W-full max-w-xl lg:w-3/4">
+          <h6 className="mb-2 font-semibold">{project.title}</h6>
+          <p className="mb-4 text-neutral-400">{project.description}</p>
+          {project.technologies.map((tech,index)=>(
+            <span key={index} className="mr-2 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800 hover:text-white transition-colors duration-200 cursor-pointer">{tech}</span>
+          ))}
+        </div>
+        </div>
+        
+      ))}
+
+    </div>
       
     </div>
   )
